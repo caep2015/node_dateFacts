8c573b9ac2ce65bd884a707abf0e5b82dfc93be8
Carlota Pearl <carlota.pearl@gmail.com>
2017-07-24 21:48:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Node.js project. Uses two packages from NPM to make a script that gives information about the current date and time. The libraries used are: Moment and Chalk.
---
+++ index.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+moment().format();
+
+let date = [year, month, day, hour, minute, second];
+
+
+
+date.forEach(function (date){
+  console.log(moment([year, month, day, hour, minute, second]));
+})
